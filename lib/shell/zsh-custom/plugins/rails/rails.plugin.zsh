6e1395a58ad7e40fdaa581e6fe981733f9c8edad
Josh Minnie <josh.minnie@gmail.com>
2024-05-24 14:21:18 -0600

    Remove shell alias in favor of git alias

--- lib/shell/zsh-custom/plugins/rails/rails.plugin.zsh
+++ lib/shell/zsh-custom/plugins/rails/rails.plugin.zsh
@@ -1,5 +1,4 @@
 # Aliases for eChecks project
 alias gup="OVERCOMMIT_DISABLE=1 git up"
-alias ginit="git init;git add .;git commit -m 'Initial commit'"
 alias rc="bundle exec rails c"
 alias rs="bundle exec rails s"
