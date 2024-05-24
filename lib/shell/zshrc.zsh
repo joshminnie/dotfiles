56c600cde03808c2f40345c0515a95c506b50681
Josh Minnie <josh.minnie@gmail.com>
2024-05-24 09:12:37 -0600

    Rename mine to local to better represent purpose

--- lib/shell/zshrc.zsh
+++ lib/shell/zshrc.zsh
@@ -70,7 +70,7 @@ ZSH_CUSTOM="$HOME/.zsh-custom"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(brew git git-prompt rake-fast rails mine)
+plugins=(brew git git-prompt rake-fast rails local)
 
 source $ZSH/oh-my-zsh.sh
 
