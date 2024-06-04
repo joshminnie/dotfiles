b224ff52f33c5d9e6dcac50a632c8e811bec281e
Josh Minnie <josh.minnie@gmail.com>
2024-06-04 13:56:09 -0600

    Add common brew libraries to path environment variable

---
+++ lib/shell/zsh-custom/env.zsh
@@ -0,0 +1,4 @@
+# Homebrew exports
+export PATH="$PATH:$(brew --prefix node@20)/bin"
+export PATH="$PATH:$(brew --prefix postgresql@15)/bin"
+export PATH="$PATH:$(brew --prefix python@3.12)/bin:$(brew --prefix python@3.12)/libexec/bin"
