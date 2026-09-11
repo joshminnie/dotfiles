08ce1845d008082aa6694bf782578c22c3d79759
Josh Minnie <T444117@deluxe.com>
2026-09-11 11:48:25 -0600

    Copy keybindings and usersettings for vscode

--- lib/vscode/install.zsh
+++ lib/vscode/install.zsh
@@ -4,3 +4,5 @@ set -e
 
 rm -rf "$HOME/Library/Application Support/Code/User/snippets"
 ln -sfh "$HOME/.dotfiles/lib/vscode/snippets" "$HOME/Library/Application Support/Code/User/snippets"
+cp -f "$HOME/.dotfiles/lib/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+cp -f "$HOME/.dotfiles/lib/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
